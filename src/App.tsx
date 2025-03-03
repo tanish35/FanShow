c053191f54e7fbb35ab277a5f1b28f0e67d7a9ce
tanish35 <tanishmajumdar2912@gmail.com>
2025-03-03 10:33:30 +0530

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import ArtistDetailsPage from "./pages/ArtistDetails";
 import Navbar from "./components/Navbar";
+import SpotifyLogin from "./pages/SpotifyLogin";
 
-// Placeholder components for other routes
 const Home = () => <h1>Home Page</h1>;
 const About = () => <h1>About Page</h1>;
 const NotFound = () => <h1>404: Page Not Found</h1>;
@@ -17,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/artist" element={<ArtistDetailsPage />} />
+          <Route path="/spotify-login" element={<SpotifyLogin />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
